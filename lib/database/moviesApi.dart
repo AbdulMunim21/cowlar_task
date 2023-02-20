9c72e936454e342fc55680e1472d03f13e90569e
AbdulMunim21 <amunim1999@gmail.com1>
2023-02-21 01:10:23 +0500

    movieModel Added

--- lib/database/moviesApi.dart
+++ lib/database/moviesApi.dart
@@ -3,6 +3,11 @@ import 'package:dio/dio.dart';
 class MoviesAPI {
   final dio = Dio();
   getAllMovies() async {
-    
+    final response = await dio.get(
+        "https://api.themoviedb.org/3/trending/all/day?api_key=576f015f7065a05f3effe8b630ea2e9c");
+
+    // save in local database
+
+    // store in provider
   }
 }
---
+++ lib/model/movieModel.dart
@@ -0,0 +1,19 @@
+class MovieModel {
+  final int id;
+  final String title;
+  final bool hasVideo;
+  final String imageUrl;
+  final String overview;
+  final String releaseDate;
+  final List genreIds;
+
+  MovieModel({
+    required this.id,
+    required this.title,
+    required this.hasVideo,
+    required this.imageUrl,
+    required this.overview,
+    required this.releaseDate,
+    required this.genreIds,
+  });
+}
